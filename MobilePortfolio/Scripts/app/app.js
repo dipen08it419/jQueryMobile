6a37cef7670f9f52916435ddcce278a9761ee550
Dipen Shah <dipen08it419@gmail.com>
2015-02-23 14:07:30 -0800

    Added script to communicate with external web service in contact section.

--- MobilePortfolio/Scripts/app/app.js
+++ MobilePortfolio/Scripts/app/app.js
@@ -8,3 +8,45 @@ $('[data-role=page]').on('pageshow', function (event, ui) {
     $("#" + event.target.id).find("[data-role=footer]").html("<span aria-level='1' role='heading' class='ui-title'>© 2015 Dipen Shah. All Rights Reserved.</span><center><a href='http://dipen08it419.github.io'>visit full site</a></center>");
     //$("#" + event.target.id).find("[data-role=navbar]").navbar();
 });
+
+//method to send message using service hosted
+function sendMessage() {
+    var emailId = $("#emailId").val();
+    var message = $("#message").val();
+
+    if (emailId && message) {
+
+        var data = $.ajax({
+            type:'GET',
+            url: 'http://aspspider.info/dipen08it419/ContactService.svc/Contact?emailId=' + emailId + '&message=' + message,
+            crossDomain: true,
+            dataType:'jsonp'
+        }).success(function (data, status) {
+            if (data) {
+                alert('Your message sent successfully!');
+
+                //resetting text
+                $("#emailId").val("");
+                $("#message").val("");
+
+                $("#emailId").focus();
+            }
+
+            console.log(data);
+        }).error(function (data, status) {
+            if (status === 'parsererror') {
+                alert('Your message sent successfully!');
+
+                //resetting text
+                $("#emailId").val("");
+                $("#message").val("");
+
+                $("#emailId").focus();
+            } else {
+                alert('Sorry! Unable to send message, please try again.');
+            }
+        });
+    } else {
+        alert('Please fill up the form.');
+    }
+}
